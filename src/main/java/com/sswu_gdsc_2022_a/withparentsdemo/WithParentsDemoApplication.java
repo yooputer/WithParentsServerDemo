4df215a54530fcdb1574b7ef4aa96a2ed8d25ff6
yooputer <yooputer@gmail.com>
2022-11-02 16:21:49 +0900

    initial commit

---
+++ src/main/java/com/sswu_gdsc_2022_a/withparentsdemo/WithParentsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sswu_gdsc_2022_a.withparentsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithParentsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithParentsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sswu_gdsc_2022_a/withparentsdemo/WithParentsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sswu_gdsc_2022_a.withparentsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithParentsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
