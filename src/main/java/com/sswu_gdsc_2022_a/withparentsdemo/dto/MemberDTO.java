117ad4803adfa33e05230d1da1841dedb55041bd
yooputer <yooputer@gmail.com>
2022-11-09 17:08:52 +0900

    [FIX] MemberDTO의 entityToDTO 메서드 수정 family가 null이면 family_id가 null이되도록 수정

--- src/main/java/com/sswu_gdsc_2022_a/withparentsdemo/dto/MemberDTO.java
+++ src/main/java/com/sswu_gdsc_2022_a/withparentsdemo/dto/MemberDTO.java
@@ -17,12 +17,13 @@ public class MemberDTO {
     private Long family_id;
 
     public static MemberDTO entityToDTO(Member e){
+
         return new MemberDTO(
                 e.getId(),
                 e.getUsername(),
                 e.getNickname(),
                 e.getAuthority(),
-                e.getFamily().getId()
+                e.getFamily() == null ? null : e.getFamily().getId()
         );
     }
 
